5c8e3d5f770ec2ceaa066a40dd7508a1e69e5293
Cristian Ramirez <ccrhdbj@outlook.com>
2021-11-25 07:08:03 -0500

    Mediante express entrega el array de la coleccion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('./infraestructura/connexiondb');
-
 const ProjectModel = require ('./models/proyectoModel');
+const express = require('express');
+const { response } = require('express');
+
+const api = express();
 
 const projectAguas = new ProjectModel({
   nombre: 'Proyecto de contaminacion de los rios',
@@ -8,7 +11,7 @@ const projectAguas = new ProjectModel({
   facultad: 'MicroBiologia'
 })
 
-projectAguas.save((err, document )=> {
+/* projectAguas.save((err, document )=> {
   if(err){
     console.log(err);
     return;
@@ -16,13 +19,19 @@ projectAguas.save((err, document )=> {
   else{
     console.log('Ready');
   }
-})
+}) */
 
 
 //Hacer consultas
 const consultaProyectos = async () => {
-  const proyectos = await ProjectModel.find({})
-  console.log(proyectos);
+  return await ProjectModel.find({/* Aqui se indica el json para hacer la busqueda */})
 }
+ 
 
-consultaProyectos()
+api.get('/proyectos', (request, response) => {
+  consultaProyectos().then(function (resultado) {
+    response.json({proyectos:resultado})
+  })
+})
+
+api.listen('8090')

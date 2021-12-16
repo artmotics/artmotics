5c2544e089960332970b3493f0d7aaa95a265fe9
abelcotes <abelcotes@hotmail.com>
2021-12-16 10:52:19 -0500

    Se ajustan queries y pagina index de avances

--- src/graphql/avances/queries.js
+++ src/graphql/avances/queries.js
@@ -1,17 +1,37 @@
 import { gql } from '@apollo/client';
 
-const GET_AVANCES = gql`
-query Avances {
+const GET_AVANCES = gql `
+query Usuarios {
     Avances {
+      _id
       fecha
       descripcion
       proyecto {
         nombre
-        estado
       }
+      creadoPor {
+        identificacion
+      }
+    }
+  } 
+
+`;
+
+const GET_AVANCE = gql `
+query Avance($_id: String!) {
+    Avance(_id: $_id) {
+      _id
+      fecha
+      descripcion
+      proyecto {
+        nombre
+      }
+      creadoPor {
+        identificacion
     }
   }
 
 `;
 
-export { GET_AVANCES}
+
+export {GET_AVANCES, GET_AVANCE } ;
--- src/pages/avances/index.jsx
+++ src/pages/avances/index.jsx
@@ -3,55 +3,65 @@ import { useQuery } from "@apollo/client";
 import { GET_AVANCES } from "graphql/avances/queries";
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
-import { Enum_Rol, Enum_EstadoUsuario } from 'utils/enums';
+//import { Enum_Rol, Enum_EstadoUsuario } from 'utils/enums';
+//import PrivateRoute from 'components/PrivateRoute';
 
 
-const IndexAvances  = () =>{
-    const {data, error, loading} = useQuery(GET_AVANCES)
+const IndexAvances = () => {
+  const { data, error, loading } = useQuery(GET_AVANCES);
 
-    useEffect(() => {
-        console.log('data servudir', data);
-    }, [data]);
+  useEffect(() => {
+    if (error) {
+      toast.error('Error consultando avances');
+    }
+  }, [error]);
 
-    return <div>
-        Datos de los avances:
-        <table className='tabla'>
-          <thead>
-            <tr>
-              <th>Fecha</th>
-              <th>Descripcion</th>
-              <th>Nombre del proyecto</th>
-              <th>Estado del proyecto</th>
-              <th>Editar</th>
-              
-            </tr>
-          </thead>
-          <tbody>
-            {data && data.Avances ? (
-              <>
-                {data.Avances.map((u) => {
-                  return (
-                    <tr key={u._id}>
-                      <td>{u.fecha}</td>
-                      <td>{u.descripcion}</td>
-                      <td>{u.proyecto.nombre}</td>
-                      <td>{u.proyecto.estado}</td>
-                      <td>
-                        <Link to={`/avances/editar/${u._id}`}>
-                          <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </>
-            ) : (
-              <div>No autorizado</div>
-            )}
-          </tbody>
-        </table>
+  if (loading) return <div>Cargando....</div>;
 
-    </div>;
+    return (
+      <div>
+      Datos de los avances:
+      <table className='tabla'>
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Descripcion</th>
+            <th>Nombre del proyecto</th>
+            <th>Documento del creador</th>
+            <th>Editar</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.Avances ? (
+            <>
+              {data.Avances.map((u) => {
+                return (
+                  <tr key={u._id}>
+                    <td>{u.fecha}</td>
+                    <td>{u.descripcion}</td>
+                    <td>{u.proyecto.nombre}</td>
+                    <td>{u.creadoPor.identificacion}</td>
+                    <td>
+                      <Link to={`/avances/editar/${u._id}`}>
+                        <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </>
+          ) : (
+            <div>No autorizado</div>
+          )}
+        </tbody>
+      </table>
+
+  </div>
+
+  )
+
+    
 };
 
 export default IndexAvances;
